89a4b4ec5a5dcb709f8a99805d7fcd9e1f97b9cb
Gustavo Eyros <gustavoeyros@hotmail.com>
2022-12-24 13:46:11 -0300

    fix: padding textarea

---
+++ dist/routes/pages.js
@@ -0,0 +1,10 @@
+const rootRoute = "/pages";
+const firstPage = "/" || "/index.html";
+const secondPage = `${rootRoute}/second-page.html`;
+const thirdPage = `${rootRoute}/third-page.html`;
+const fourthPage = `${rootRoute}/fourth-page.html`;
+const fifthPage = `${rootRoute}/fifth-page.html`;
+const sixthPage = `${rootRoute}/sixth-page.html`;
+import * as pageRoutes_1 from "../routes/pages";
+export { pageRoutes_1 as pageRoutes };
+//# sourceMappingURL=pages.js.map
